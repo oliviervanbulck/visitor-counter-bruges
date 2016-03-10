c2c922638d032d7a760dc90c9b1bd9124106ea27
oliviervanbulck <olivier@vanbulck.com>
2016-03-10 14:22:40 +0100

    Added status page for tessel

--- routes/status.js
+++ routes/status.js
@@ -1,3 +1,11 @@
-/**
- * Created by danvybiral on 08/03/16.
- */
+var express = require('express');
+var router = express.Router();
+
+var counter = require('../counter.js');
+
+router.get('/', function(req, res){
+    res.send('' + counter.getStatus());
+    console.log("==========  Status Request  ==========");
+});
+
+module.exports = router;
